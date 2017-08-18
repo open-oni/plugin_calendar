5ed676f11472d94a6e89660379a61985c487e566
Jeremy Echols <jechols@uoregon.edu>
2017-08-18 16:54:56 -0700

    WIP - all-issues calendar year dropdown

--- urls.py
+++ urls.py
@@ -3,17 +3,16 @@ from onisite.plugins.calendar import views
 
 urlpatterns = [
     # Override core's /issues, which is the calendar for the first year that
-    # has any content ingested.  Note that the final slash is optional since we
-    # may one day make all the core URLs more consistent.
-    url(r'^issues/?$', views.issues, name="calendar_issues"),
+    # has any content ingested
+    url(r'^issues/$', views.all_issues_calendar, name="calendar_all_issues_calendar"),
 
     # Override core's calendar for a user-specified year
-    url(r'^issues/(?P<year>\d{4})/?$', views.issues, name="calendar_issues_for_year"),
+    url(r'^issues/(?P<year>\d{4})$', views.all_issues_calendar, name="calendar_all_issues_calendar_for_year"),
 
     # Override core's title-specific calendar for the first year available
-    url(r'^lccn/(?P<lccn>\w+)/issues/?$', views.issues_title, name="calendar_issues_title"),
+    url(r'^lccn/(?P<lccn>\w+)/issues/$', views.title_issues_calendar, name="calendar_title_issues_calendar"),
 
     # Override core's title-specific calendar for a user-specified year
-    url(r'^lccn/(?P<lccn>\w+)/issues/(?P<year>\d{4})/?$',
-       views.issues_title, name="calendar_issues_title_for_year"),
+    url(r'^lccn/(?P<lccn>\w+)/issues/(?P<year>\d{4})$',
+       views.title_issues_calendar, name="calendar_title_issues_calendar_for_year"),
 ]
--- views.py
+++ views.py
@@ -1,21 +1,39 @@
+from django import forms
 from django.conf import settings
 from core import models
 from core.decorator import cache_page
 from django.shortcuts import get_object_or_404, render
 
 @cache_page(settings.DEFAULT_TTL_SECONDS)
-def issues(request, year=None):
+def all_issues_calendar(request, year=None):
     issues = models.Issue.objects.all().order_by('date_issued')
     page_title = "Browse All Issues"
     page_name = "issues"
     crumbs = list(settings.BASE_CRUMBS)
-    return render(request, 'issues_calendar.html', locals())
+    select_year_form = _issues_year_select(issues, year)
+    return render(request, 'all_issues_calendar.html', locals())
 
 @cache_page(settings.DEFAULT_TTL_SECONDS)
-def issues_title(request, lccn, year=None):
+def title_issues_calendar(request, lccn, year=None):
     title = get_object_or_404(models.Title, lccn=lccn)
     issues = title.issues.all()
     page_title = "Browse Issues: %s" % title.display_name
     page_name = "issues_title"
     crumbs = create_crumbs(title)
-    return render(request, 'issues_title_calendar.html', locals())
+    select_year_form = _issues_year_select(issues, year)
+    return render(request, 'title_issues_calendar.html', locals())
+
+def _issues_year_select(issues, start_year):
+    if issues.count() > 0:
+        if start_year is None:
+            start_year = issues[0].date_issued.year
+        else:
+            start_year = int(start_year)
+    else:
+        start_year = 1900
+    dates = issues.dates('date_issued', 'year')
+
+    class SelectYearForm(forms.Form):
+        year = forms.fields.ChoiceField(choices=((d.year, d.year) for d in dates), initial=start_year)
+
+    return SelectYearForm()
