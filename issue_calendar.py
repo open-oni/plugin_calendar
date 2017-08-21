6dbd106394cea23d7f0a231ec3115d094041ede1
Jeremy Echols <jechols@uoregon.edu>
2017-08-21 11:42:46 -0700

    Put year dropdown into IssueCalendar

--- issue_calendar.py
+++ issue_calendar.py
@@ -1,17 +1,64 @@
 import calendar
 
+from django import forms
+from django.db import connection
+from django.db.models import Count
+
+from core import models
+
 class IssueCalendar(calendar.Calendar):
-    """
-    This calendar returns complete HTML pages.
-    """
-
-    def __init__(self, firstweekday=0, issues=None, all_issues=False):
-        calendar.Calendar.__init__(self, firstweekday)
-        self.issues = issues
-        self.all_issues = all_issues
-
     # CSS classes for the day <td>s
     cssclasses = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+
+    def __init__(self, title, year):
+        calendar.Calendar.__init__(self, 6)
+        self.title = title
+        self.year = year
+        self._get_issue_years()
+        self._normalize_selected_year()
+        self._get_issue_dates_for_selected_year()
+
+    def _get_issue_years(self):
+        """Set up an array of years and issue counts for each year"""
+        cursor = connection.cursor()
+        select = "SELECT year(date_issued) AS issue_year, COUNT(id) FROM core_issue"
+        where = ""
+        params = []
+        if self.title is not None:
+            where = "WHERE title_id = %s"
+            params = [self.title.lccn]
+        grouporder = "GROUP BY issue_year ORDER BY issue_year"
+        cursor.execute("%s %s %s" % (select, where, grouporder), params)
+        self.yeardata = cursor.fetchall()
+
+    def _normalize_selected_year(self):
+        """Determine the selected year in case one was not already selected"""
+        if len(self.yeardata) > 0:
+            if self.year is None:
+                self.year = self.yeardata[0][0]
+            else:
+                self.year = int(self.year)
+        else:
+            self.year = 1900
+
+    def _get_issue_dates_for_selected_year(self):
+        """Get a count of issues for each day of the selected year"""
+        self.date_counts = {}
+        qs = models.Issue.objects.filter(date_issued__year = self.year)
+        if self.title is not None:
+            qs = qs.filter(title_id = self.title.lccn)
+        qs = qs.annotate(num_issues = Count("date_issued", distinct = True))
+        for issue in qs.all():
+            self.date_counts[issue.date_issued] = issue.num_issues
+
+    def year_form(self):
+        class SelectYearForm(forms.Form):
+            year = forms.fields.ChoiceField(
+                choices=((yd[0], "%d (%d)" % (yd[0], yd[1])) for yd in self.yeardata),
+                initial=self.year
+            )
+
+        return SelectYearForm()
 
     def formatday(self, year, month, day, weekday):
         """
--- views.py
+++ views.py
@@ -1,5 +1,3 @@
-from django import forms
-from django.db import connection
 from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 
@@ -7,12 +5,14 @@ from core import models
 from core.decorator import cache_page
 from core.utils.utils import create_crumbs
 
+from issue_calendar import IssueCalendar
+
 @cache_page(settings.DEFAULT_TTL_SECONDS)
 def all_issues_calendar(request, year=None):
     page_title = "Browse All Issues"
     page_name = "issues"
     crumbs = list(settings.BASE_CRUMBS)
-    select_year_form = _issues_year_select(None, year)
+    issue_calendar = IssueCalendar(None, year)
     return render(request, 'all_issues_calendar.html', locals())
 
 @cache_page(settings.DEFAULT_TTL_SECONDS)
@@ -21,37 +21,5 @@ def title_issues_calendar(request, lccn, year=None):
     page_title = "Browse Issues: %s" % title.display_name
     page_name = "issues_title"
     crumbs = create_crumbs(title)
-    select_year_form = _issues_year_select(title, year)
+    issue_calendar = IssueCalendar(title, year)
     return render(request, 'title_issues_calendar.html', locals())
-
-def _issues_year_select(title, start_year):
-    yeardata = _all_issue_years(title)
-    if len(yeardata) > 0:
-        if start_year is None:
-            start_year = yeardata[0][0]
-        else:
-            start_year = int(start_year)
-    else:
-        start_year = 1900
-
-    class SelectYearForm(forms.Form):
-        year = forms.fields.ChoiceField(
-            choices=((yd[0], "%d (%d)" % (yd[0], yd[1])) for yd in yeardata),
-            initial=start_year
-        )
-
-    return SelectYearForm()
-
-def _all_issue_years(title):
-    cursor = connection.cursor()
-    select = "SELECT year(date_issued) AS issue_year, COUNT(id) FROM core_issue"
-    where = ""
-    params = []
-    if title is not None:
-        where = "WHERE title_id = %s"
-        params = [title.lccn]
-    grouporder = "GROUP BY issue_year ORDER BY issue_year"
-    cursor.execute("%s %s %s" % (select, where, grouporder), params)
-    yeardata = cursor.fetchall()
-
-    return yeardata
